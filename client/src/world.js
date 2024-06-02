25bca2979b5f2df9c862f34992818d6c43e6fbc2
N.R. <nrush@drew.edu>
2024-06-01 21:13:48 -0400

    Corrected async import
    Fixed the issue with loading in the meshes and creating the Restocker

--- client/src/world.js
+++ client/src/world.js
@@ -10,16 +10,29 @@ export class World {
     //Need to finsih modifying how the world imports the restock icons
     constructor(scene) {
         this.scene = scene;
-        SceneLoader.ImportMeshAsync("", "", "./assets/restaurant_furnishing1.glb", scene, (meshes) => {
-            meshes.forEach(mesh => {
+        SceneLoader.ImportMeshAsync("", "./", "./assets/restaurant_furnishing1.glb", scene).then((result) => {
+            const meshes = result.meshes;
+            meshes.forEach((mesh) => {
                 mesh.isPickable = false;
                 mesh.enablePointerMoveEvents = false;
             });
             this.DURGS = meshes[0];
             this.DURGS.name = "restaurant";
-        }).then((scene) => {
             new Restocker(scene);
+        }).catch((error) => {
+            console.log("Loading mesh error: ", error);
         });
+        // this.scene = scene;
+        // SceneLoader.ImportMeshAsync("", "", "./assets/restaurant_furnishing1.glb", scene, (meshes) => {
+        //     meshes.forEach(mesh => {
+        //         mesh.isPickable = false;
+        //         mesh.enablePointerMoveEvents = false;
+        //     });
+        //     this.DURGS = meshes[0];
+        //     this.DURGS.name = "restaurant";
+        // }).then((scene) => {
+        //     new Restocker(scene);
+        // });
         new Bun(scene, true, new Vector3(0, 1, 0));
     }
 }
