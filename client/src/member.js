48c654b3bf5697a58c3f93d68c5f78a2c8af8901
N.R. <nrush@drew.edu>
2024-06-22 15:00:46 -0400

    Other Players LERP
    Other players now LERP on the client side

--- client/src/member.js
+++ client/src/member.js
@@ -7,12 +7,16 @@ export class Member {
     scene;
     right_hand = "";
     grab = false;
+    NEXT_POSITION;
+    PREVIOUS_POSITION;
 
     constructor(username, scene, position, texture) {
         this.username = username;
         this.movement = new TransformNode(username, scene);
         this.movement.position = new Vector3(0, 0, 0);
         this.movement.position = position;
+        this.NEXT_POSITION = new Vector3(0,0,0);
+        this.PREVIOUS_POSITION = new Vector3(0,0,0);
         this.scene = scene;
         SceneLoader.ImportMesh("body", "", "./assets/player.glb", this.scene, (meshes) => {
             if (meshes.length > 0) {
@@ -42,19 +46,28 @@ export class Member {
     }
 
     render() {
+        const delta = this.scene.getEngine().getDeltaTime() / 1000;
+        const interpolationFactor = Math.min(1, delta * 60);
+        Vector3.LerpToRef(this.movement.position, this.NEXT_POSITION, interpolationFactor, this.movement.position);
         if (this.right_hand) {
             this.right_hand.metadata.classInstance.body.transformNode.position.set(this.movement.position.x, this.movement.position.y, this.movement.position.z);
         }
     }
 
     updatePosition(position, rotation) {
+        this.PREVIOUS_POSITION = this.NEXT_POSITION.clone();
+        this.movement.position = this.PREVIOUS_POSITION.clone();
+        this.NEXT_POSITION = new Vector3(position._x, position._y, position._z);
+        this.movement.rotation = new Vector3(0, rotation._y, 0);
+    }
+    updatePosition2(position, rotation) {
         console.log(position);
         this.movement.position = new Vector3(position._x, position._y, position._z);
         if (this.right_hand) {
             // this.right_hand.parent.parent.position = this.movement.position;
             this.right_hand.metadata.classInstance.body.transformNode.position.set(this.movement.position.x, this.movement.position.y, this.movement.position.z);
         }
-        this.movement.rotation = new Vector3(0, rotation._y,0);
+        this.movement.rotation = new Vector3(0, rotation._y, 0);
     }
 
     updateGrab(item) {
